c85502ecebf7752c99db4ead20060f98e752ba2a
AleR17 <ale.xandrarus@yahoo.com>
2016-04-22 00:58:40 +0300

    Prima_incercare.

---
+++ Snake/src/com/alexandra/snake/Snake.java
@@ -0,0 +1,16 @@
+package com.alexandra.snake;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Snake extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_snake);
+	}
+
+	
+}
